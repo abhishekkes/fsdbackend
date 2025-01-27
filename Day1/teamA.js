4d48f2b42efa7104a938537eb64b22680be2586e
Abhishek Kesharwani <abhishekkesharwani689@gmail.com>
2025-01-27 15:40:56 +0530

    Day1

---
+++ Day1/teamA.js
@@ -0,0 +1,9 @@
+let sum=(a,b)=>{return (a+b)};
+let dif=(a,b)=>{return (a-b)};
+
+const ob={
+    sum: sum,
+    dif: dif
+}
+
+module.exports = ob;
---
+++ Day1/teamB.js
@@ -0,0 +1,4 @@
+const obj=require('./teamA.js');
+
+const a =obj.sum(12,34);
+console.log(a)
