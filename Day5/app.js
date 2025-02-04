6f84e8ff13f6bb8b50981b0bd1889c2a7b7ef024
Abhishek Kesharwani <abhishekkesharwani689@gmail.com>
2025-02-04 08:52:26 +0530

    Day5

--- Day5/app.js
+++ Day5/app.js
@@ -1,18 +1,98 @@
-const express=require('express');
-const {connectDB} = require('./config/dbConfig.js');
-const Product = require('./models/productModel.js');
-const app=express();
+const express = require("express");
+const { connectDB } = require("./config/dbConfig.js");
+const Product = require("./models/productModel.js");
+const app = express();
 app.use(express.json());
-app.post('/products',(req,res) => {
-    res.send('Server is working');
+app.post("/products", async (req, res) => {
+//   res.send("Server is working");
+  try {
     const newProduct = req.body;
-    Product.create(newProduct);
-    res.send('Product added successfully');
-    res.status(200);
-    
-    
+    await Product.create(newProduct);
+    console.log("---debug----");
+    res.status(201);
+    res.json({ message: "Product added successfully" });
+  } catch (error) {
+    console.log(error.message);
+    console.log(error._message);
+    console.log(error.name);
+    if (error.name == "ValidationError") {
+      res
+        .status(400)
+        .json({ status: "fail", message: "Data validation Failed" });
+    } else {
+      res.status(500).json({ status: "fail", message: "Server Error" });
+    }
+  }
 });
 
-app.listen(8000,() => {
-    console.log('Server is running on port 8000');
+// app.get('/api/products',async (req, res) => {
+//     try{
+//         const products = await Product.find({});
+//         res.status(200);
+//         res.json(products);
+//     }catch(e){
+//         console.error(e);
+//         res.status(500).send('Server Error');
+//     }
+// });
+
+// app.get('/api/products/:name',async (req, res) => {
+//     try{
+//         const product = await Product.findOne({ name: req.params.name });
+//         if(!product) return res.status(404).json({ message: 'Product not found' });
+//         res.json(product);
+//     }catch(e){
+//         console.error(e);
+//         res.status(500).send('Server Error');
+//     }
+
+// });
+
+// app.get("/api/products", async (req, res) => {
+//     try {
+//         const { q = "" } = req.query;  // Get search query from request
+//         console.log(q);
+
+//         let productQuery = Product.find(); // Initialize query
+
+//         if (q.length > 0) {
+//             const reg = new RegExp(q, "i");  // Case-insensitive regex
+//             productQuery = productQuery.where('name').regex(reg); 
+//         }
+
+//         const products = await productQuery.exec();  // Execute the query
+//         res.json(products);
+
+//     } catch (e) {
+//         console.error(e);
+//         res.status(500).send('Server Error');
+//     }
+// });
+
+app.get("/api/products", async (req, res) => {
+    try {
+        const { q = "",size=4,page=1 } = req.query;  // Get search query from request
+        console.log(q);
+
+        let productQuery = Product.find(); // Initialize query
+
+        if (q.length > 0) {
+            const reg = new RegExp(q, "i");  // Case-insensitive regex
+            productQuery = productQuery.where('name').regex(reg); 
+        }
+        productQuery.sort("name -title");
+        productQuery.skip((page-1)*size);
+        productQuery.limit(size);  // Limit the number of products per page
+        const products = await productQuery.exec();  // Execute the query
+        res.json(products);
+
+    } catch (e) {
+        console.error(e);
+        res.status(500).send('Server Error');
+    }
+});
+
+
+app.listen(8000, () => {
+  console.log("Server is running on port 8000");
 });
