1808396ac1deac632fcf4ec4d1281cb899a33c22
Abhishek Kesharwani <abhishekkesharwani689@gmail.com>
2025-02-04 08:51:53 +0530

    updated

---
+++ Day6/models/productModel.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const productSchema =new mongoose.Schema(
+    {
+        discount: Number,
+        company: String,
+        title: String,
+        quantity: {type: Number,default:1,min:0},
+        thumbnail: String
+    }
+);
+
+const Product= mongoose.model('products',productSchema);
+console.log("Model created");
+module.exports=Product;    
